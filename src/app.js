7421216001cbb2479f0e013888ddf1f79c59f19a
Vũ Quang Duy <vduy2350@gmail.com>
2022-03-08 14:07:49 +0700

    update buoi 1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var a = 20;
+var b = 100;
+var age = 19;
+var name = "Vũ Q Duy";
+var status = true;
+var info = { 1: , "bạc":  };
+var stringArr = ["a", "b", "b"];
+var numberArr = [1, 3, 4, 5];
+var objectArr = [{ "a": 1 }, { "a": 2 }];
+function sum(numA, numB) {
+    return numA + numB;
+}
+console.log(sum(a, b));
+console.log(age);
---
+++ src/app.ts
@@ -0,0 +1,23 @@
+export{}
+
+type Product = {
+    id: number,
+    name: string,
+}
+
+const a: number = 20;
+const b: number = 100;
+const age: number = 19;
+const name: string = "Vũ Q Duy";
+const status: boolean = true;
+const info: Product = { id: 1 ,name: "bạc"};
+const stringArr: string[] = ["a", "b", "b"];
+const numberArr: number[] = [1,3,4,5];
+const objectArr: Product[]=[{id: 1, name: "duy" }]
+
+
+function sum(numA: number, numB: number) :number{
+    return numA + numB;
+}
+
+// document.querySelector("#app").innerHTML = "<h2> Age: " + age + "</h2>" + "<h2> Name: " + name + "</h2>" + "<h2> status: " + status + "</h2>" + "<h2> info: " + info.name + info.id + "</h2>";
