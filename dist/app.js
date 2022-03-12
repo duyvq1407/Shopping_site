b6fb6088ee452f16321d84a9b9f4376158deb5b1
Vũ Quang Duy <vduy2350@gmail.com>
2022-03-12 12:49:18 +0700

    update

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = 20;
-const b = 100;
-const age = 19;
-const name = "Vũ Q Duy";
-const status = true;
-const info = { id: 1, name: "bạc" };
-const stringArr = ["a", "b", "b"];
-const numberArr = [1, 3, 4, 5];
-const objectArr = [{ id: 1, name: "duy" }];
-function sum(numA, numB) {
-    return numA + numB;
+// type Product = {
+//     id: number,
+//     name: string,
+// }
+// const a: number = 20;
+// const b: number = 100;
+// const age: number = 19;
+// const name: string = "Vũ Q Duy";
+// const status: boolean = true;
+// const info: Product = { id: 1 ,name: "bạc"};
+// const stringArr: string[] = ["a", "b", "b"];
+// const numberArr: number[] = [1,3,4,5];
+// const objectArr: Product[]=[{id: 1, name: "duy" }]
+// function sum(numA: number, numB: number) :number{
+//     return numA + numB;
+// }
+// document.querySelector("#app").innerHTML = `<h2> Age: ${age} </h2> <h2> Name:  ${name} </h2> <h2> status:  ${status} </h2> <h2> info:  ${info.name} ${info.id} </h2>`;
+function show(a, b) {
+    return [a, b];
 }
-// document.querySelector("#app").innerHTML = "<h2> Age: " + age + "</h2>" + "<h2> Name: " + name + "</h2>" + "<h2> status: " + status + "</h2>" + "<h2> info: " + info.name + info.id + "</h2>";
+show("a", "c");
+show(5, "a");
+show({ id: 1, name: "duy" }, 5);
