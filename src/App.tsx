7cb6c7cd6a0f3521b1a5f97c9526f69857fe3ce8
Vũ Quang Duy <vduy2350@gmail.com>
2022-03-12 13:57:42 +0700

    update buoi 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,26 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import ShowInfo from './components/showInfo'
+import Product from './components/product'
+
+interface IProduct{
+    id: number, 
+    name: string
+}
+
+const data = [
+  { id: 1, name: "Product A"}, // item
+  { id: 2, name: "Product B"}, // item
+]
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [products, setProducts] = useState<IProduct[]>(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        <ShowInfo name="abc" age={10}/>
+        {products.map(item => <Product data={item} />)}
     </div>
   )
 }
